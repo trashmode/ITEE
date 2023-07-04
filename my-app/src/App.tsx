c7e8548214a5eb6f9c3fa6a0eac2f3f50de5cd55
Michael Chai <michaelchai@Michaels-TrashBook-Pro.local>
2023-07-04 10:40:40 +1000

    finished constructors for classes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoadingPage from './Pages/LoadingPage';
 
 function App() {
   return (
--- my-app/src/Components/room.tsx
+++ my-app/src/Components/room.tsx
@@ -2,4 +2,8 @@ import { Session } from "./sessions";
 export class Room {
     id: String;
     session: Session;
+    constructor(r: Room) {
+        this.id = r.id;
+        this.session = r;
+    }
 }
