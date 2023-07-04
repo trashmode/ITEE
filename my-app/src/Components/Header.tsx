49e9db106686e0a686d7a4292b7fa2eda47da530
Daniel Duan <daniel.duan2226@gmail.com>
2023-07-04 11:17:49 +1000

    added

--- my-app/src/Components/Header.tsx
+++ my-app/src/Components/Header.tsx
@@ -7,7 +7,7 @@ import {
     Toolbar,
     Typography,
   } from '@mui/material';
-  import styled from "styled-components";
+  import styled from 'styled-components';
   
 
 
--- my-app/src/Pages/Homepage.tsx
+++ my-app/src/Pages/Homepage.tsx
@@ -4,10 +4,10 @@ import Connectbox from '../Components/Connectbox'
 const Homepage = () => {
     return (
                 
-                <div style = {{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                    <Connectbox title = "Connect here!"/>
+            <div style = {{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                <Connectbox title = "Connect here!"/>
                 </div>
-    )
+    );
 }
 
 export default Homepage;
